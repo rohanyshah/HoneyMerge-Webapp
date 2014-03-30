4d0d053f8f89667515b4dc78b428e834ed25e81f
Rohan Shah <rohanyshah@me.com>
2014-03-30 16:19:21 -0400

    test

--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -1,4 +1,79 @@
-// Listen to swipe events
+function KeyboardInputManager() {
+  this.events = {};
+
+  if (window.navigator.msPointerEnabled) {
+    //Internet Explorer 10 style
+    this.eventTouchstart    = "MSPointerDown";
+    this.eventTouchmove     = "MSPointerMove";
+    this.eventTouchend      = "MSPointerUp";
+  } else {
+    this.eventTouchstart    = "touchstart";
+    this.eventTouchmove     = "touchmove";
+    this.eventTouchend      = "touchend";
+  }
+
+  this.listen();
+}
+
+KeyboardInputManager.prototype.on = function (event, callback) {
+  if (!this.events[event]) {
+    this.events[event] = [];
+  }
+  this.events[event].push(callback);
+};
+
+KeyboardInputManager.prototype.emit = function (event, data) {
+  var callbacks = this.events[event];
+  if (callbacks) {
+    callbacks.forEach(function (callback) {
+      callback(data);
+    });
+  }
+};
+
+KeyboardInputManager.prototype.listen = function () {
+  var self = this;
+
+  var map = {
+    39: 2, // Right
+    37: 3, // Left
+    87: 0, // W
+    69: 1, // E
+    65: 2, // A
+    68: 3, // D
+    90: 4, // Z
+    88: 5  // X
+  };
+
+  document.addEventListener("keydown", function (event) {
+    var modifiers = event.altKey || event.ctrlKey || event.metaKey ||
+                    event.shiftKey;
+    var mapped    = map[event.which];
+
+    if (!modifiers) {
+      if (mapped !== undefined) {
+        event.preventDefault();
+        self.emit("move", mapped);
+      }
+
+      if (event.which === 32){
+        var game_restart = confirm('Are you sure?');
+        if(game_restart == true){
+          self.restart.bind(self)(event);
+        }
+      };
+    }
+  });
+
+  var retry = document.querySelector(".retry-button");
+  retry.addEventListener("click", this.restart.bind(this));
+  retry.addEventListener(this.eventTouchend, this.restart.bind(this));
+
+  var keepPlaying = document.querySelector(".keep-playing-button");
+  keepPlaying.addEventListener("click", this.keepPlaying.bind(this));
+  keepPlaying.addEventListener("touchend", this.keepPlaying.bind(this));
+
+  // Listen to swipe events
   var touchStartClientX, touchStartClientY;
   var gameContainer = document.getElementsByClassName("game-container")[0];
 
@@ -43,4 +118,14 @@
       self.emit("move", absDx > absDy ? (dx > 0 ? 3 : 2) : (dy > 0 ? (dx > 0 ? 5 : 4) : (dx > 0 ? 1 : 0)));
     }
   });
+};
+
+KeyboardInputManager.prototype.restart = function (event) {
+  event.preventDefault();
+  this.emit("restart");
+};
+
+KeyboardInputManager.prototype.keepPlaying = function (event) {
+  event.preventDefault();
+  this.emit("keepPlaying");
 };
